ec94488fe8eae1d52dbfa3c38bc254767ff899a9
Gustavo <gustavo.felipe01@outlook.com>
2021-08-11 12:24:16 -0300

    Remove php

--- encontracliente/js/script.js
+++ encontracliente/js/script.js
@@ -6,8 +6,7 @@ const action = {
     export: document.getElementById("export"),
     send: document.getElementById("zapzap"),
     checkAll: document.getElementById("selectAll")
-}
-const exe = {
+}, exe = {
     exportAll: () => {
         console.log("AAAA");
     },
@@ -15,27 +14,27 @@ const exe = {
         console.log("AAAA");
     },
     pintaCard: (x, y) => {
-        if(y == true){
+        if (y == true) {
             btn.card[x].classList.add("cardSelected");
-        }else{
+        } else {
             btn.card[x].classList.remove("cardSelected");
         }
     },
     renameBtn: () => {
         let count = document.querySelectorAll('.check:checked');
-        if(btn.card.length == count.length){
+        if (btn.card.length == count.length) {
             action.checkAll.innerText = "Remove All";
-        }else{
+        } else {
             action.checkAll.innerText = "Select All";
         }
     },
     btnSelect: (v) => {
 
-        if(btn.check[v].checked == false){
+        if (btn.check[v].checked == false) {
             btn.check[v].checked = true;
 
             exe.pintaCard(v, true)
-        }else{
+        } else {
             btn.check[v].checked = false;
             exe.pintaCard(v, false)
         }
@@ -44,24 +43,48 @@ const exe = {
     chekedAll: () => {
         var define;
 
-        if(action.checkAll.innerText == "Remove All"){
+        if (action.checkAll.innerText == "Remove All") {
             define = false;
-        }else{
+        } else {
             define = true;
         }
 
-        for(x = 0; x < btn.check.length; x ++){
+        for (x = 0; x < btn.check.length; x++) {
             btn.check[x].checked = define;
             exe.pintaCard(x, define);
         }
-        
+
         exe.renameBtn();
     }
 
 
+}, api = {
+    guiafacil: (search) => {
+        const data = JSON.stringify({
+            "pesquisa": search,
+        });
+
+        let settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "http://localhost/codigo/",
+            "method": "POST",
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "processData": false,
+            "data": data
+        };
+
+        $.ajax(settings).done(function(response){
+            console.log(response);
+        });
+    }
 }
 
 //Ações de Click da página
 action.export.addEventListener("click", exe.exportAll, true);
 action.send.addEventListener("click", exe.sendAll, true);
 action.checkAll.addEventListener("click", exe.chekedAll, true)
+
+api.guiafacil("estetica");
