afa3adb2bb4c4d756da702ea415432119a89b395
Gustavo <gustavo.felipe01@outlook.com>
2021-08-11 12:01:45 -0300

    Encontra Clientes

---
+++ encontracliente/js/script.js
@@ -0,0 +1,67 @@
+var btn = {
+    check: document.querySelectorAll(".check"),
+    card: document.querySelectorAll(".card"),
+}
+const action = {
+    export: document.getElementById("export"),
+    send: document.getElementById("zapzap"),
+    checkAll: document.getElementById("selectAll")
+}
+const exe = {
+    exportAll: () => {
+        console.log("AAAA");
+    },
+    sendAll: () => {
+        console.log("AAAA");
+    },
+    pintaCard: (x, y) => {
+        if(y == true){
+            btn.card[x].classList.add("cardSelected");
+        }else{
+            btn.card[x].classList.remove("cardSelected");
+        }
+    },
+    renameBtn: () => {
+        let count = document.querySelectorAll('.check:checked');
+        if(btn.card.length == count.length){
+            action.checkAll.innerText = "Remove All";
+        }else{
+            action.checkAll.innerText = "Select All";
+        }
+    },
+    btnSelect: (v) => {
+
+        if(btn.check[v].checked == false){
+            btn.check[v].checked = true;
+
+            exe.pintaCard(v, true)
+        }else{
+            btn.check[v].checked = false;
+            exe.pintaCard(v, false)
+        }
+        exe.renameBtn();
+    },
+    chekedAll: () => {
+        var define;
+
+        if(action.checkAll.innerText == "Remove All"){
+            define = false;
+        }else{
+            define = true;
+        }
+
+        for(x = 0; x < btn.check.length; x ++){
+            btn.check[x].checked = define;
+            exe.pintaCard(x, define);
+        }
+        
+        exe.renameBtn();
+    }
+
+
+}
+
+//Ações de Click da página
+action.export.addEventListener("click", exe.exportAll, true);
+action.send.addEventListener("click", exe.sendAll, true);
+action.checkAll.addEventListener("click", exe.chekedAll, true)
---
+++ encontracliente/readme.md
@@ -0,0 +1,8 @@
+## Instalar o SASS 
+npm install -g sass
+
+## Comando para compilar o SASS 
+sass scss/index.scss css/style.css
+
+## Compila em tempo real
+sass --watch scss/index.scss css/style.css
